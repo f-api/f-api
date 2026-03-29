af7ecb172b0ee99a2caca026046e3db5ae95b1aa
Nhahan <kisy324@naver.com>
2026-03-30 00:37:05 +0900

    Switch quiz code blocks to dark IntelliJ theme

--- quiz/source/src/main.jsx
+++ quiz/source/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "highlight.js/styles/intellij-light.css";
+import "highlight.js/styles/base16/darcula.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
